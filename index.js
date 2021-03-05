d9e0404acc8c113d56c96941b54ba03cb051f3d7
khang-nd <khang.nguyenduyvn@gmail.com>
2021-03-05 23:39:31 +0700

    added favicon

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = require("express")();
 
 const port = process.env.PORT || 3000;
 
-app.get("/favicon.ico", (_, res) => res.status(204));
 app.get("/", getStats);
 app.listen(port, () =>
   console.info("Server running at: http://localhost:" + port)
